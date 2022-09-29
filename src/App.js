5d8c3bb560da881bc59c9cb981290f456dabb7b0
acefail <daoxuandat92tn@gmail.com>
2022-09-29 10:10:23 +0700

    Feature update: Add category card item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Card from './components/card/Card'
+import FAIcon from './components/icon/FontAwesomeIcon';
 
 function App() {
     return (
         <div className="App">
-            <Card />
+            <Card name="Drinks" amount="5" backgroundColor="yellow" icon={<FAIcon name={"fa fa-beer"}></FAIcon>}/>
+            <Card name="Drinks" amount="5" backgroundColor="violet" icon={<FAIcon name={"fa fa-beer"}></FAIcon>}/>
         </div>
     );
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import './Card.css';
 
-const Card = () => {
-    return (
-        <div className='card-container'>
-            
+function Card({name, amount, backgroundColor, icon}) {
+
+
+    const styles = {
+        background: {
+            backgroundColor: backgroundColor,
+        },
+    };
+
+
+
+  return (
+    <div className='card-container' >
+        <div className='card-body' style={styles.background}> 
+            {icon}
+            <div className='category-name'>
+                <span>{name}</span>
+            </div>
+            <div className='category-amount'>
+                <span>{amount} items</span>
+            </div>
         </div>
-    );
+    </div>
+  )
 }
 
 export default Card;
