85520334bb1abf06a301bf2c3019f8154e7ee765
Tri <lecaominhtri0701@gmail.com>
2022-10-16 00:13:52 +0700

    feat: finished navbar

--- src/App.js
+++ src/App.js
@@ -2,27 +2,21 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Dashboard from './pages/Dashboard.jsx';
+import Menu from './pages/Menu.jsx';
 import About from './pages/About.jsx';
-import Analytics from './pages/Analytics.jsx';
-import Comment from './pages/Comment.jsx';
 import Product from './pages/Product.jsx';
-import ProductList from './pages/ProductList.jsx';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar>
+      <Navbar />
+      <div className="container">
         <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/comment" element={<Comment />} />
-          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/" element={<Menu />} />
           <Route path="/product" element={<Product />} />
-          <Route path="/productList" element={<ProductList />} />
+          <Route path="/about" element={<About />} />
         </Routes>
-      </Navbar>
+      </div>
     </BrowserRouter>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,67 +1,28 @@
-import React, { useState } from 'react';
-import {
-    FaTh,
-    FaBars,
-    FaUserAlt,
-    FaRegChartBar,
-    FaCommentAlt,
-    FaShoppingBag,
-    FaThList
-}from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
+export default function Navbar() {
+  return (
+    <div className="nav">
+      <Link to="/" className="site-title">
+        POS System
+      </Link>
+      <ul>
+        <CustomLink to="/product">Product</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+      </ul>
+    </div>
+  )
+}
 
-const Sidebar = ({children}) => {
-    const menuItem=[
-        {
-            path:"/",
-            name:"Dashboard",
-            icon:<FaTh/>
-        },
-        {
-            path:"/about",
-            name:"About",
-            icon:<FaUserAlt/>
-        },
-        {
-            path:"/analytics",
-            name:"Analytics",
-            icon:<FaRegChartBar/>
-        },
-        {
-            path:"/comment",
-            name:"Comment",
-            icon:<FaCommentAlt/>
-        },
-        {
-            path:"/product",
-            name:"Product",
-            icon:<FaShoppingBag/>
-        },
-        {
-            path:"/productList",
-            name:"Product List",
-            icon:<FaThList/>
-        }
-    ]
-    return (
-        <div className="container">
-           <div style={{width: "200px"}} className="sidebar">
-               <div className="top_section">
-                   <h1 style={{display: "block"}} className="logo">Logo</h1>
-               </div>
-               {
-                   menuItem.map((item, index)=>(
-                       <NavLink to={item.path} key={index} className="link" >
-                           <div className="icon">{item.icon}</div>
-                           <div style={{display: "block"}} className="link_text">{item.name}</div>
-                       </NavLink>
-                   ))
-               }
-           </div>
-           <main>{children}</main>
-        </div>
-    );
-};
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
 
-export default Sidebar;
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
