3ba65441d4006276594c243c1e983141b06d9732
Trần Đình Gia Hải <60392917+giathyex@users.noreply.github.com>
2022-10-17 17:04:06 +0700

    Add table box

--- src/pages/Table/Table.jsx
+++ src/pages/Table/Table.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Table.css';
 
 const Table = () => {
     return (
-        <div className='Table'>
-            <h1>Table</h1>
+        <div id="maintable">
+            <div id="leftbank">
+                <p class="tablebox" id="rcorners1">1</p>
+                <p class="tablebox" id="rcorners2">2</p>
+                <p class="tablebox" id="rcorners3">3</p>
+                <p class="tablebox" id="rcorners4">4</p>
+                <p class="tablebox" id="rcorners5">5</p>
+            </div>
+            <div id="rightbank">
+                <p class="tablebox" id="rcorners6">6</p>
+                <p class="tablebox" id="rcorners7">7</p>
+                <p class="tablebox" id="rcorners8">8</p>
+                <p class="tablebox" id="rcorners9">9</p>
+                <p class="tablebox" id="rcorners10">10</p>
+            </div>
         </div>
     );
 };
