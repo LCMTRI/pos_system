a9ea0644a80385229f70af6809a1de7bea53e80e
Tri <lecaominhtri0701@gmail.com>
2022-10-17 16:36:40 +0700

    feat: change font, change Table to Reservation and add right section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Intro from './pages/Intro/Intro';
 import Menu from './pages/Menu/Menu';
-import Table from './pages/Table/Table';
+import Reservation from './pages/Reservation/Reservation';
 import Dashboard from './pages/Dashboard/Dashboard'
 import Employee from './pages/Employee/Employee';
 
@@ -13,14 +13,19 @@ const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
-      <div className="container">
+      <div className="mid-container">
         <Routes>
           <Route path="/" element={<Intro />} />
           <Route path="/menu" element={<Menu />} />
-          <Route path="/table" element={<Table />} />
+          <Route path="/reservation" element={<Reservation />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/employee" element={<Employee />} />
         </Routes>
+      </div>
+      <div className="right-container">
+        <h1>Side-section</h1>
+        <br />
+        <h3>In progress...</h3>
       </div>
     </BrowserRouter>
   );
