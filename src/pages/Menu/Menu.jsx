476d580e96d2386a70b27ab7136bf77655995f64
Tri <lecaominhtri0701@gmail.com>
2022-10-18 17:51:16 +0700

    feat: add side section

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Card from '../../components/card/Card';
+import Foodcard from '../../components/foodCard/FoodCard';
+import FAIcom from '../../components/icon/FontAwesomeIcon';
 
 const Menu = () => {
     return (
         <div>
-            <h1>Menu page</h1>
+            <Card name="Haha xerath go boom boom" amount={10} backgroundColor="#cfdddb" icon={<FAIcom name='fa fa-beer'></FAIcom>} />
+            <br />
+            <Foodcard name="ur mom is fat" price={420} from="huhu" to="haha" />
         </div>
     );
 };
