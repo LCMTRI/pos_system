fba315995a98b16c74f2fbff8aec4dc477781ccd
Leino <80114093+Leino2604@users.noreply.github.com>
2022-10-28 00:24:34 +0700

    Add files via upload

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 import { Pie, Bar, Line } from 'react-chartjs-2'
 import './Dashboard.css'
 
+import { Chart, ArcElement } from 'chart.js'
+import 'chart.js/auto';
+Chart.register(ArcElement);
+
 const Dashboard = () => {
     return (
         <div>
-            <h1>Dashboard page</h1>
-            <br></br>
-
             <div className='card-number'>
                 <div className='first-card'>
                     Tổng doanh thu: <br></br>
-                    <a className='number'>335.24</a> triệu đồng
+                    <span className='number'>335.24</span> <br></br> triệu đồng
                 </div>
                 <div className='second-card'>
                     Tổng lượt khách vào quán: <br></br>
-                    <a className='number'>9,402</a> lượt
+                    <span className='number'>9,402</span> <br></br> lượt
                 </div>
                 <div className='third-card'>
                     Số nhân viên làm việc hôm nay: <br></br>
-                    <a className='number'>13</a>
+                    <span className='number'>13</span> <br></br> người
                 </div>
             </div>
 
@@ -37,6 +38,17 @@ const Dashboard = () => {
                                 hoverOffset: 3
                             }]
                         }}
+                        options={{
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: 'Tỷ lệ doanh thu các loại đồ ăn 7 ngày gần nhất'
+                                },
+                                legend: {
+                                    position: 'right'
+                                }
+                            }
+                        }}
                     />
                 </div>
                 <div className='second-chart'>
@@ -44,7 +56,7 @@ const Dashboard = () => {
                         data={{
                             labels: ['Red', 'Orange', 'Yellow'],
                             datasets: [{
-                                label: 'Top 3 món bán chạy trong 30 ngày gần nhất',
+                                // label: 'Top 3 món bán chạy trong 30 ngày gần nhất',
                                 axis: 'y',
                                 data: [90, 88, 84],
                                 fill: false,
@@ -56,25 +68,76 @@ const Dashboard = () => {
                             }],
                         }}
                         options={{
-                            indexAxis: 'y'
+                            indexAxis: 'y',
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: 'Top 3 món bán chạy trong 30 ngày gần nhất'
+                                },
+                                legend: {
+                                    display: false
+                                }
+                            }
                         }}
-                        
+
                     />
                 </div>
-                {/* <div className='third-chart'>
+                <div className='third-chart'>
                     <Line
                         data={{
-
+                            labels: ['10/10', '11/10', '12/10', '13/10', '14/10', '15/10', '16/10', '17/10'],
+                            datasets: [{
+                                label: 'Doanh thu ngày theo 7 ngày gần nhất',
+                                data: [33000000, 22000000, 32000000, 13000000, 55000000, 21000000, 43000000, 45000000],
+                                backgroundColor: [
+                                    'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Gray', 'Black'
+                                ],
+                                hoverOffset: 3
+                            }]
                         }}
+                        options={{
+                            maintainAspectRatio: false,
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: 'Doanh thu ngày theo 7 ngày gần nhất'
+                                },
+                                legend: {
+                                    display: false
+                                }
+                            }
+                        }}
+
                     />
                 </div>
                 <div className='fourth-chart'>
                     <Line
                         data={{
-
+                            labels: ['10/10', '11/10', '12/10', '13/10', '14/10', '15/10', '16/10', '17/10'],
+                            datasets: [{
+                                label: 'Lượng khách đến quán theo 7 ngày gần nhất',
+                                data: [33, 22, 32, 13, 55, 21, 43, 45],
+                                backgroundColor: [
+                                    'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Gray', 'Black'
+                                ],
+                                hoverOffset: 3
+                            }]
                         }}
+                        options={{
+                            maintainAspectRatio: false,
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: 'Lượng khách đến quán theo 7 ngày gần nhất'
+                                },
+                                legend: {
+                                    display: false
+                                }
+                            }
+                        }}
+
                     />
-                </div> */}
+                </div>
             </div>
         </div>
     );
